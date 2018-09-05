93fdb01b06ccf9ff73bdf33c70e43c667cfd1dcd
Andrzej Kwiecien <andrzej.kwiecien@gugik.gov.pl>
2018-09-05 11:32:08 +0200

    added VwCar

---
+++ src/main/java/VwCar.java
@@ -0,0 +1,17 @@
+public class VwCar implements ICar {
+
+    private String name ;
+
+    public VwCar(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+
+        return this.name;
+    }
+
+    public String drive() {
+        return this.name+" is driving";
+    }
+}
